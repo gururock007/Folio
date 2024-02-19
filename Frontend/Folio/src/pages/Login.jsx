3b12df6a83c95a0a48df3ddc7357a29bd010d0ef
gururock007 <gurushiktemp@gmail.com>
2024-02-20 01:28:09 +0530

    Sign UP Page Completed

---
+++ Frontend/Folio/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignCard } from '../components/SignCard'
+import Header from '../components/Header'
+
+export const Login = () => {
+  return (
+    <div>
+        <Header />
+        <SignCard />
+    </div>
+  )
+}
