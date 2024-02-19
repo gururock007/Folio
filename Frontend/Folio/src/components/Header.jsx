62c5bb56334870e75401e5ce656cba96dd98cba2
d-man1212 <itsmedharshan.shankar@gmail.com>
2024-02-19 20:55:41 +0530

    Basic Header

--- Frontend/Folio/src/components/Header.jsx
+++ Frontend/Folio/src/components/Header.jsx
@@ -1,28 +1,79 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import dark from "../assets/dark.png";
 import light from "../assets/light.png";
-// import ThemeSwitch from "./ThemeSwitch";
 
-export default function Header() {
+const Header = () => {
   const navigate = useNavigate();
+  const [isDark, setIsDark] = useState(
+    localStorage.getItem("theme") === "dark"
+  );
+
+  const toggleTheme = () => {
+    const newIsDark = !isDark;
+    setIsDark(newIsDark);
+    document.body.classList.toggle("dark");
+    localStorage.setItem("theme", newIsDark ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    setIsDark(savedTheme === "dark");
+
+    if (savedTheme === "dark") {
+      document.body.classList.add("dark");
+    }
+  }, []);
+
   return (
     <div>
-      <nav>
-        <div onClick={() => navigate("/")}>
-          <img
-            src={document.body.classList.contains("dark") ? dark : light}
-            alt="Logo"
-          />
-          Folio
+      <nav className="bg-background p-4">
+        <div className="grid grid-cols-2 items-center">
+          <div className="flex items-center" onClick={() => navigate("/")}>
+            <img src={isDark ? dark : light} alt="Logo" className="h-8 w-8" />
+            <span className="text-white text-xl font-bold ml-2">Folio</span>
+          </div>
+          <ul className="flex justify-end space-x-16 ">
+            <li
+              onClick={() => navigate("/")}
+              className="bg-secondary cursor-pointer text-primary px-4 py-2 transition duration-300 ease-in-out hover:text-background rounded-md"
+            >
+              Home
+            </li>
+            <li
+              onClick={() => navigate("/books")}
+              className="text-text cursor-pointer"
+            >
+              Books
+            </li>
+            <li
+              onClick={() => navigate("/lists")}
+              className="text-text cursor-pointer"
+            >
+              Lists
+            </li>
+            <li
+              onClick={() => navigate("/login")}
+              className="bg-primary cursor-pointer text-secondary px-4 py-2 transition duration-300 ease-in-out hover:text-background rounded-md"
+            >
+              Login
+            </li>
+            <label className="inline-flex items-center cursor-pointer">
+              <input
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+                checked={isDark}
+                onChange={toggleTheme}
+              />
+              <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600"></div>
+              <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+            </label>
+          </ul>
         </div>
-        <ul>
-          <li onClick={() => navigate("/")}>Home</li>
-          <li onClick={() => navigate("/books")}>Books</li>
-          <li onClick={() => navigate("/lists")}>Lists</li>
-          <li onClick={() => navigate("/login")}>Login</li>
-          {/* <ThemeSwitch /> */}
-        </ul>
       </nav>
     </div>
   );
-}
+};
+
+export default Header;
--- Frontend/Folio/src/components/Hero.jsx
+++ Frontend/Folio/src/components/Hero.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import { PrimaryButton } from './PrimaryButton'
+import { PrimaryButton } from "./PrimaryButton";
 
 export const Hero = () => {
   return (
     <>
-        <div className=' text-white'>
-            <div className=' container'>
-                <div className=' grid grid-cols-3 pb-12 pt-4'>
-                    <div className=' col-span-2 py-16 mx-16 font-semibold'>
-                        <div className=' text-5xl font-Poppins '>This Is Dummy Text Lorem Ipsum Insta </div>
-                    </div>
-                    <div className=' col-span-2 mx-16  font-light'>
-                        this is another Dummy text From the UI Designer< br/>for Beautiful design
-                    </div>
-                    <div className=' col-span-2 mx-16 py-5'>
-                        <PrimaryButton props ={{ text : "Sign up" }} />
-                    </div>
-                </div>
+      <div className=" text-white">
+        <div className=" container">
+          <div className=" grid grid-cols-3 pb-12 pt-4">
+            <div className=" col-span-2 py-16 mx-16 font-semibold">
+              <div className=" text-5xl font-Poppins ">
+                This Is Dummy Text Lorem Ipsum Insta{" "}
+              </div>
             </div>
+            <div className=" col-span-2 mx-16  font-light">
+              this is another Dummy text From the UI Designer
+              <br />
+              for Beautiful design
+            </div>
+            <div className=" col-span-2 mx-16 py-5">
+              <PrimaryButton props={{ text: "Sign up" }} />
+            </div>
+          </div>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
