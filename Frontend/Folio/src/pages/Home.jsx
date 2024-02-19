37708d1f9e947e2bdcf8cae821ecefa23109813e
d-man1212 <itsmedharshan.shankar@gmail.com>
2024-02-19 16:16:44 +0530

    Created Header

---
+++ Frontend/Folio/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+export default function Home() {
+  return (
+    <div>
+      <Header className="text-primary" />
+      TEST
+      <Header />
+      <Header />
+    </div>
+  );
+}
