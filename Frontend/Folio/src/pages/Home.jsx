94f8be3bdf23227e844e6c9f558b23d816859c26
gururock007 <gurushiktemp@gmail.com>
2024-02-20 12:39:01 +0530

    Added Chat Bot To the Project

--- Frontend/Folio/src/pages/Home.jsx
+++ Frontend/Folio/src/pages/Home.jsx
@@ -5,6 +5,8 @@ import { PageInfo } from "../components/PageInfo";
 import { DashBoard } from "./DashBoard";
 import { useAuth } from "../contexts/AuthContext";
 import { Book } from "./Book";
+import { Results } from "./Results";
+import ChatBot from "../components/ChatBot";
 export default function Home() {
   const { currentUser } = useAuth();
   if (currentUser) {
@@ -13,9 +15,10 @@ export default function Home() {
     return (
       <div>
         <Header />
-        {/* <Hero />
-        <PageInfo /> */}
-        <Book />
+        <Hero />
+        <PageInfo />
+        <ChatBot />
+        {/* <Results /> */}
         <Footer />
       </div>
     );
